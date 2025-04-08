b9158d3fd03d05fb69f43a8745ee8e218378b757
23001082 <163306416+23001082@users.noreply.github.com>
2025-04-08 12:24:26 +0200

    Add files via upload

---
+++ SammysRentalPriceJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sammysrentalprice.java;
+
+/**
+ *
+ * @author 23001082
+ */
+public class SammysRentalPriceJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
